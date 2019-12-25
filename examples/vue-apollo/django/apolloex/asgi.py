9d08ab6652cd0963178b0c9c66121f6b16c05fd7
Patryk Bratkowski <git@patryk.tech>
2019-12-25 15:47:31 +0200

    feat: format vue-apollo example using black

--- examples/vue-apollo/django/apolloex/asgi.py
+++ examples/vue-apollo/django/apolloex/asgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apolloex.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "apolloex.settings")
 
 application = get_asgi_application()
--- examples/vue-apollo/django/apolloex/wsgi.py
+++ examples/vue-apollo/django/apolloex/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'apolloex.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "apolloex.settings")
 
 application = get_wsgi_application()
